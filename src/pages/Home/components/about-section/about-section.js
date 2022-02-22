091bd039dbcb7b9361003fb5c58ca3e86424d699
TabishAK <takchamp6@gmail.com>
2022-02-22 19:37:34 +0500

    All Completed

---
+++ src/pages/Home/components/about-section/about-section.js
@@ -0,0 +1,41 @@
+import icon1 from "../../../../images/about-us/2.png";
+import icon2 from "../../../../images/about-us/3.png";
+import icon3 from "../../../../images/about-us/4.png";
+import "./about-section.scss";
+
+const AboutSection = () => {
+  return (
+    <div className="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-6">
+            <h1>About Us</h1>
+            <p>
+              At Gary Manufacturing, all production is handled in the USA. For
+              over 60 years we have maintained quality production standards,
+              exceptional customer service, and multi-generational employee
+              support.
+            </p>
+            <button className="custom-products">Custom Products</button>
+          </div>
+          <div className="col-xl-6">
+            <div className="appraisal appraisal-1">
+              <img src={icon1} alt="icon-1" />
+              <h5>In Buisness, since 1958</h5>
+            </div>
+            <div className="appraisal appraisal-2">
+              <img src={icon2} alt="icon-1" />
+              <h5>Women in manufacturing</h5>
+            </div>
+            <div className="appraisal appraisal-3">
+              <img src={icon3} alt="icon-1" />
+              <h5>Consistent, sustainable Growth</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -5,19 +5,87 @@ import video from "../../videos/machine.mp4";
 import "./home.scss";
 import Banner from "./components/Banner/banner";
 import Carousel from "./../../components/carousel/carousel";
+import React from "react";
+import ProjectSection from "./components/project-section/project-section";
+import AboutSection from "./components/about-section/about-section";
+import logo from "../../images/logo/roklin-black.png";
+import { FaFacebookF, FaYoutube, FaTwitter, FaLinkedin } from "react-icons/fa";
+import { ImLocation2 } from "react-icons/im";
+
 const Home = () => {
   return (
     <div className="home">
       <div className="video">
         <ReactPlayer url={video} muted playing loop />
       </div>
-
       <NavHeader />
       <Navbar />
-
       <Banner />
-
       <Carousel />
+      <ProjectSection />
+      <AboutSection />
+
+      <div className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-3">
+              <img src={logo} alt="" />
+              <div className="logos">
+                <FaFacebookF />
+                <FaTwitter />
+                <FaYoutube />
+                <FaLinkedin />
+              </div>
+            </div>
+            <div className="col-xl-3"></div>
+            <div className="col-xl-3">
+              <ul>
+                <h6>
+                  <ImLocation2 />
+                  Address
+                </h6>
+                <li
+                  style={{
+                    fontWeight: 500,
+                    marginBottom: 5,
+                  }}
+                >
+                  2626 Southport Way, Suite E <br />
+                  National City, CA 91950
+                </li>
+                <li>Tel: 800-775-0804</li>
+                <li>Email: sales@garymanufacturing.com</li>
+              </ul>
+            </div>
+            <div className="col-xl-3">
+              <ul>
+                <h6>Links</h6>
+                <li>About Us </li>
+                <li>Materials</li>
+                <li>Case Studies</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer-bottom">
+          <div className="container">
+            <div className="links">
+              <ul>
+                <li>Terms of Service</li>
+                <li>Privacy Policy</li>
+                <li> Sitemap</li>
+              </ul>
+            </div>
+            <div className="created-by">
+              <p>
+                © 2022 Gary Manufacturing, All Rights Reserved | Site created by
+                Thomas Marketing Services.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
